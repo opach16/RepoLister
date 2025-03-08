3a6399031e4f9d5e6fcac1209d130963009e39a3
Konrad Opach <opach16@outlook.com>
2025-03-08 14:50:16 +0100

    added @JsonProperty annotations

--- src/main/java/com/atipera/repolister/data/BranchResponse.java
+++ src/main/java/com/atipera/repolister/data/BranchResponse.java
@@ -1,9 +1,12 @@
 package com.atipera.repolister.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class BranchResponse {
+    @JsonProperty("branch_name")
     private final String branchName;
+    @JsonProperty("commit_sha")
     private final String sha;
 }
--- src/main/java/com/atipera/repolister/data/RepoResponse.java
+++ src/main/java/com/atipera/repolister/data/RepoResponse.java
@@ -1,12 +1,16 @@
 package com.atipera.repolister.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class RepoResponse {
+    @JsonProperty("repository_name")
     private final String name;
+    @JsonProperty("owner_login")
     private final String login;
+    @JsonProperty("branch")
     private final List<BranchResponse> branchResponses;
 }
